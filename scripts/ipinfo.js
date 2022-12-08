4367694d80c3b1be8cb32e95f1f76a9a20ebdbf9
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2022-12-07 22:53:24 -0500

    Update ipinfo.js

--- scripts/ipinfo.js
+++ scripts/ipinfo.js
@@ -1,6 +1,7 @@
 function ipinfo() {
+  fetch("https://ip.seeip.org/json/").then(res => res.json()).then(data => Ip = data['ip'])
+  fetch(`https://vpnapi.io/api/${Ip}/`).then(res => res.json()).then(data => IpLog = data)
+  //fetch(`https://ipapi.co/${Ip}/json/`).then(res => res.json()).then(data => IpLog2 = data)
   
-  
-  
+  return IpLog
 }
-  
