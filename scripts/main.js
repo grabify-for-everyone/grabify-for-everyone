000a19887b39d4730b544142cb7984d598881795
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2022-12-09 21:59:12 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
 const generate = function() {
   var url = document.getElementById("url").value
-  var xhr = new XMLHttpRequest()
-  xhr.open("POST", "https://grabify-webserver.tympanicblock612.repl.co/generate", true)
-  xhr.send(JSON.stringify({"url":url}))
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://grabify-webserver.tympanicblock612.repl.co/generate');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.responseType = 'json';
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log(xhr.response);
+      window.resp = xhr.response
+    } else {
+      console.error(xhr.response);
+    }
+  };
+  xhr.onerror = () => {
+    console.error('An error occurred while making the request');
+  };
+  const body = JSON.stringify({"url":url});
+  xhr.send(body);
+  
 }
