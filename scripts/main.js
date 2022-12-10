860b99828d5fac4b44c02aae180b654b7be83336
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2022-12-09 20:16:07 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,5 @@ const generate = function() {
   var url = document.getElementById("url").value
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "https://grabify-webserver.tympanicblock612.repl.co/generate", true)
-  xhr.send(JSON.stringify({"url":url, "ip":Ip}))
+  xhr.send(JSON.stringify({"url":url}))
 }
