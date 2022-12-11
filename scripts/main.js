e97caaf410d51a2cf18a1f7164ec9ed90e94d617
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2022-12-10 23:57:12 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,45 @@
 const generate = function() {
   var url = document.getElementById("url").value
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', 'https://grabify-webserver.tympanicblock612.repl.co/generate');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.responseType = 'json';
+  var xhr = new XMLHttpRequest()
+  xhr.open('POST', 'https://grabify-webserver.tympanicblock612.repl.co/generate')
+  xhr.setRequestHeader('Content-Type', 'application/json')
+  xhr.responseType = 'json'
   xhr.onload = () => {
     if (xhr.status === 200) {
-      console.log(xhr.response);
+      console.log(xhr.response)
       window.resp = xhr.response
     } else {
-      console.error(xhr.response);
+      console.error(xhr.response)
     }
   };
   xhr.onerror = () => {
-    console.error('An error occurred while making the request');
+    console.error('An error occurred while making the request')
   };
-  const body = JSON.stringify({"url":url});
-  xhr.send(body);
+  const body = JSON.stringify({"url":url})
+  xhr.send(body)
+}
+const view = function() {
+  var sessionId = document.getElementById("view").value
+  var xhr = new XMLHttpRequest()
+  xhr.open("GET", `https://grabify-webserver.tympanicblock612.repl.co/get/${sessionId}`)
+  xhr.responseType = 'json'
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log(xhr.response)
+      window.view = xhr.response
+    } else {
+      console.error(xhr.response)
+    }
+  };
+  xhr.onerror = () => {
+    console.error('An error occurred while making the request')
+  };
+  xhr.send()
+  
+  for (var n in window.view) {
+    document.createElement()
+  }
+  
+  
   
 }
