ed5c6fdebfbd08a7b1091cbb5556436ce55ef829
tympanicblock61 <99092380+tympanicblock61@users.noreply.github.com>
2022-12-10 23:51:50 -0500

    Delete ipinfo.js

--- scripts/ipinfo.js
+++
@@ -1,8 +0,0 @@
-function ipinfo() {
-  fetch("https://ip.seeip.org/json/").then(res => res.json()).then(data => Ip = data['ip'])
-  fetch(`https://vpnapi.io/api/${Ip}/`).then(res => res.json()).then(data => IpLog = data)
-  IpLog["userAgent"] = navigator.userAgent
-  //fetch(`https://ipapi.co/${Ip}/json/`).then(res => res.json()).then(data => IpLog2 = data)
-  
-  return IpLog
-}
